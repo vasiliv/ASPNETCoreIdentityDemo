777f6576480f41d55e3541d5db05e59b4b665272
vasiliv <32122468+vasiliv@users.noreply.github.com>
2024-08-19 13:02:49 +0400

    part 5 finished - https://dotnettutorials.net/lesson/login-and-logout-in-asp-net-core-identity/

--- ASPNETCoreIdentityDemo/Controllers/AccountController.cs
+++ ASPNETCoreIdentityDemo/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using ASPNETCoreIdentityDemo.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -51,6 +52,47 @@ namespace ASPNETCoreIdentityDemo.Controllers
                 }
             }
             return View(model);
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    // Handle successful login
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+                if (result.RequiresTwoFactor)
+                {
+                    // Handle two-factor authentication case
+                }
+                if (result.IsLockedOut)
+                {
+                    // Handle lockout scenario
+                }
+                else
+                {
+                    // Handle failure
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View(model);
+                }
+            }
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("index", "home");
         }
     }
 }
